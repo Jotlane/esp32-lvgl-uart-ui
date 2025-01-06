967dd8fa5ce59b3e1c5fadf006b03eddd3622165
Jotlane <55050270+Jotlane@users.noreply.github.com>
2025-01-06 17:02:00 +0800

    initial commit

---
+++ main/CrowPanel_ESP32_7.0.cpp
@@ -0,0 +1,250 @@
+#include <lvgl.h>
+#include <demos/lv_demos.h>
+#include <DHT20.h>
+#include <lvgl.h>
+//UI
+#include "ui.h"
+
+#include <LovyanGFX.hpp>
+#include <lgfx/v1/platforms/esp32s3/Panel_RGB.hpp>
+#include <lgfx/v1/platforms/esp32s3/Bus_RGB.hpp>
+
+#include <SPI.h>
+SPIClass &spi = SPI;
+
+int led;
+int activate_test;
+DHT20 dht20;
+
+static uint32_t screenWidth;
+static uint32_t screenHeight;
+static lv_disp_draw_buf_t draw_buf;
+static lv_color_t disp_draw_buf[800 * 480 / 10];
+//static lv_color_t disp_draw_buf;
+static lv_disp_drv_t disp_drv;
+
+#include <Ticker.h> //Call the ticker. H Library
+Ticker ticker1;
+#include <Arduino_GFX_Library.h>
+#define TFT_BL 2
+#define GFX_BL DF_GFX_BL // default backlight pin, you may replace DF_GFX_BL to actual backlight pin
+
+/* More dev device declaration: https://github.com/moononournation/Arduino_GFX/wiki/Dev-Device-Declaration */
+
+class LGFX : public lgfx::LGFX_Device
+{
+public:
+
+  lgfx::Bus_RGB     _bus_instance;
+  lgfx::Panel_RGB   _panel_instance;
+
+  LGFX(void)
+  {
+
+
+    {
+      auto cfg = _bus_instance.config();
+      cfg.panel = &_panel_instance;
+      
+      cfg.pin_d0  = GPIO_NUM_15; // B0
+      cfg.pin_d1  = GPIO_NUM_7;  // B1
+      cfg.pin_d2  = GPIO_NUM_6;  // B2
+      cfg.pin_d3  = GPIO_NUM_5;  // B3
+      cfg.pin_d4  = GPIO_NUM_4;  // B4
+      
+      cfg.pin_d5  = GPIO_NUM_9;  // G0
+      cfg.pin_d6  = GPIO_NUM_46; // G1
+      cfg.pin_d7  = GPIO_NUM_3;  // G2
+      cfg.pin_d8  = GPIO_NUM_8;  // G3
+      cfg.pin_d9  = GPIO_NUM_16; // G4
+      cfg.pin_d10 = GPIO_NUM_1;  // G5
+      
+      cfg.pin_d11 = GPIO_NUM_14; // R0
+      cfg.pin_d12 = GPIO_NUM_21; // R1
+      cfg.pin_d13 = GPIO_NUM_47; // R2
+      cfg.pin_d14 = GPIO_NUM_48; // R3
+      cfg.pin_d15 = GPIO_NUM_45; // R4
+
+      cfg.pin_henable = GPIO_NUM_41;
+      cfg.pin_vsync   = GPIO_NUM_40;
+      cfg.pin_hsync   = GPIO_NUM_39;
+      cfg.pin_pclk    = GPIO_NUM_0;
+      cfg.freq_write  = 15000000;
+
+      cfg.hsync_polarity    = 0;
+      cfg.hsync_front_porch = 40;
+      cfg.hsync_pulse_width = 48;
+      cfg.hsync_back_porch  = 40;
+      
+      cfg.vsync_polarity    = 0;
+      cfg.vsync_front_porch = 1;
+      cfg.vsync_pulse_width = 31;
+      cfg.vsync_back_porch  = 13;
+
+      cfg.pclk_active_neg   = 1;
+      cfg.de_idle_high      = 0;
+      cfg.pclk_idle_high    = 0;
+
+      _bus_instance.config(cfg);
+    }
+            {
+      auto cfg = _panel_instance.config();
+      cfg.memory_width  = 800;
+      cfg.memory_height = 480;
+      cfg.panel_width  = 800;
+      cfg.panel_height = 480;
+      cfg.offset_x = 0;
+      cfg.offset_y = 0;
+      _panel_instance.config(cfg);
+    }
+    _panel_instance.setBus(&_bus_instance);
+    setPanel(&_panel_instance);
+
+  }
+};
+
+
+LGFX lcd;
+
+#include "touch.h"
+
+/* Display flushing */
+/***void my_disp_flush(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p)
+{
+    uint32_t w = (area->x2 - area->x1 + 1);
+    uint32_t h = (area->y2 - area->y1 + 1);
+
+#if (LV_COLOR_16_SWAP != 0)
+    lcd.draw16bitBeRGBBitmap(area->x1, area->y1, (uint16_t *)&color_p->full, w, h);
+#else
+    lcd.draw16bitRGBBitmap(area->x1, area->y1, (uint16_t *)&color_p->full, w, h);
+#endif
+
+    lv_disp_flush_ready(disp);
+}
+***/
+void my_disp_flush(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p)
+{
+
+  uint32_t w = (area->x2 - area->x1 + 1);
+  uint32_t h = (area->y2 - area->y1 + 1);
+  
+
+  //lcd.fillScreen(TFT_WHITE);
+#if (LV_COLOR_16_SWAP != 0)
+ lcd.pushImageDMA(area->x1, area->y1, w, h,(lgfx::rgb565_t*)&color_p->full);
+#else
+  lcd.pushImageDMA(area->x1, area->y1, w, h,(lgfx::rgb565_t*)&color_p->full);//
+#endif
+
+  lv_disp_flush_ready(disp);
+
+}
+
+
+void my_touchpad_read(lv_indev_drv_t *indev_driver, lv_indev_data_t *data)
+{
+    if (touch_has_signal())
+    {
+        if (touch_touched())
+        {
+            data->state = LV_INDEV_STATE_PR;
+
+            /*Set the coordinates*/
+            data->point.x = touch_last_x;
+            data->point.y = touch_last_y;
+            Serial.print("Data x :");
+            Serial.println(touch_last_x);
+
+            Serial.print("Data y :");
+            Serial.println(touch_last_y);
+        }
+        else if (touch_released())
+        {
+            data->state = LV_INDEV_STATE_REL;
+        }
+    }
+    else
+    {
+        data->state = LV_INDEV_STATE_REL;
+    }
+}
+
+uint16_t calData[5] = {190, 3679, 382, 3335, 0};
+
+
+
+extern "C" void app_main()
+{
+    // put your setup code here, to run once:
+    Serial.begin(9600); // Init Display
+    Wire.begin(22, 21);
+    dht20.begin();
+    lcd.begin();
+    lcd.fillScreen(BLACK);
+    lcd.setTextSize(2);
+    delay(200);
+
+    pinMode(TFT_BL, OUTPUT);
+    digitalWrite(TFT_BL, HIGH);
+
+    pinMode(38, OUTPUT);
+    digitalWrite(38, HIGH);
+
+    lv_init();
+    touch_init();
+    screenWidth = lcd.width();
+    screenHeight = lcd.height();
+
+    lv_disp_draw_buf_init(&draw_buf, disp_draw_buf, NULL, screenWidth * screenHeight / 10);
+    //  lv_disp_draw_buf_init(&draw_buf, disp_draw_buf, NULL, 480 * 272 / 10);
+    /* Initialize the display */
+    lv_disp_drv_init(&disp_drv);
+    /* Change the following line to your display resolution */
+    disp_drv.hor_res = screenWidth;
+    disp_drv.ver_res = screenHeight;
+    disp_drv.flush_cb = my_disp_flush;
+    disp_drv.draw_buf = &draw_buf;
+    lv_disp_drv_register(&disp_drv);
+
+    /* Initialize the (dummy) input device driver */
+    static lv_indev_drv_t indev_drv;
+    lv_indev_drv_init(&indev_drv);
+    indev_drv.type = LV_INDEV_TYPE_POINTER;
+    indev_drv.read_cb = my_touchpad_read;
+    lv_indev_drv_register(&indev_drv);
+
+    lcd.fillScreen(BLACK);
+    ui_init(); //主UI界面
+    Serial.println("Setup done");
+    int yes = 0;
+    while (1)
+    {
+        Serial.print(led);
+        char DHT_buffer[6];
+        //int a = (int)dht20.getTemperature();
+        //int b = (int)dht20.getHumidity();
+        int a = 10;
+        snprintf(DHT_buffer, sizeof(DHT_buffer), "%d", a);
+        lv_label_set_text(ui_Label1, DHT_buffer);
+        //snprintf(DHT_buffer, sizeof(DHT_buffer), "%d", b);
+        lv_label_set_text(ui_Label2, DHT_buffer);
+        if(led == 1)
+            digitalWrite(38, HIGH);
+        if(led == 0)
+            digitalWrite(38, LOW);
+        yes = !yes;
+        Serial.println("yes: ");
+        Serial.print(yes);
+        snprintf(DHT_buffer, sizeof(DHT_buffer), "%d", yes);
+        lv_label_set_text(ui_Label2, DHT_buffer);
+        lv_timer_handler();
+        if(activate_test)
+        {
+          activate_test = 0;
+          lv_obj_t * label = lv_label_create(ui_Panel1);
+          lv_label_set_text(label, "Inside Container!");
+        }
+        delay(5);
+    }
+}
